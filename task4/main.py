3c4af55585476c09e0bd72343d565d113bd0f964
Vfif <gmariya6666@gmail.com>
2020-08-21 15:57:58 +0300

    request with urllib with decompression

--- task4/main.py
+++ task4/main.py
@@ -1,12 +1,24 @@
-import requests
+# import requests
+#
+# url = "http://www.stackoverflow.com"
+# http_headers = {'Accept-Encoding': 'gzip'}
+#
+# response = requests.get(url, headers=http_headers)
+# print(response.headers)
+# print(response.headers['Content-Encoding'])
+# f = open('text.txt', 'wb')
+# f.write(response.content)
+# f.close()
+
+import gzip
+import urllib.request
 
 url = "http://www.stackoverflow.com"
 http_headers = {'Accept-Encoding': 'gzip'}
 
-response = requests.get(url, headers=http_headers)
-print(response.headers)
-print(response.headers['Content-Encoding'])
-# decompressed_data = zlib.decompress(response.content)
+request = urllib.request.Request(url, headers=http_headers)
+response = urllib.request.urlopen(request)
+result = gzip.decompress(response.read())
 f = open('text.txt', 'wb')
-f.write(response.content)
+f.write(result)
 f.close()
