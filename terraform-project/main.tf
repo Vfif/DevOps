f397c5ec47059aa9b5d813e9c9a99021b348c862
Vfif <gmariya6666@gmail.com>
2020-09-04 10:38:49 +0300

    create vpc-subnets-rds-ec2

--- terraform-project/main.tf
+++ terraform-project/main.tf
@@ -1,5 +1,4 @@
 terraform {
-  required_version = ">= 0.12"
   backend "s3" {
     bucket = "testt-buckett"
     region = "us-east-1"
@@ -11,15 +10,50 @@ provider "aws" {
   region = "us-east-1"
 }
 
-/*--------------------VPC------------------*/
-module "test-vpc-with-subnets" {
-  source = "./modules/vpc-with-subnets"
+--------------------VPC------------------
+
+
+resource "aws_vpc" "test-vpc" {
+  cidr_block = "172.32.0.0/16"
 }
-/*-------------------------RDS-------------------------------*/
+
+resource "aws_internet_gateway" "test-gw" {
+  vpc_id = aws_vpc.test-vpc.id
+}
+
+resource "aws_subnet" "test-public-subnet" {
+  vpc_id = aws_vpc.test-vpc.id
+  cidr_block = "172.32.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "test1-public-subnet" {
+  vpc_id = aws_vpc.test-vpc.id
+  cidr_block = "172.32.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1f"
+}
+
+resource "aws_route_table" "test-rt" {
+  vpc_id = aws_vpc.test-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.test-gw.id
+  }
+}
+
+resource "aws_route_table_association" "test-rt-association"{
+  subnet_id = aws_subnet.test-public-subnet.id
+  route_table_id = aws_route_table.test-rt.id
+}
+
+-------------------------RDS-------------------------------
+
 
 resource "aws_db_subnet_group" "test-subnet-group" {
   name       = "test-sg"
-  subnet_ids = module.test-vpc-with-subnets.subnets
+  subnet_ids = [aws_subnet.test-public-subnet.id, aws_subnet.test1-public-subnet.id]
 }
 
 resource "aws_db_instance" "test-db" {
@@ -35,10 +69,11 @@ resource "aws_db_instance" "test-db" {
   password             = "password"
 }
 
-/*-------------------------EC2-----------------------------*/
+-------------------------EC2-----------------------------
+
 
 resource "aws_instance" "test-instance" {
-  subnet_id = module.test-vpc-with-subnets.subnets[0]
+  subnet_id = aws_subnet.test-public-subnet.id
   ami           = "ami-02354e95b39ca8dec"
   instance_type = "t2.micro"
 }
